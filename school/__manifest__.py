0caeffb2b268eb482180e1ea271f9a0f95286b52
David <davalq05@gmail.com>
2022-02-04 12:35:21 +0100

    COMMIT 0402 1

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -1,30 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "school",
+    'name': "MiPruebaSchool",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'description': "La descripcion larga del modulo",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "David Alvarez",
+    'website': "http://www.paginainventada123.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Pruebas Modulos Odoo',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
